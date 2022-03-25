dd24e3e0ce09f8653b36e5766be3cb061e051fdf
NMarcR42 <nezoutmarc@hotmail.fr>
2022-03-25 16:52:18 +0100

    Création des formulaires

---
+++ app/src/main/java/com/example/amphitryon/GestionPlatActivity.java
@@ -0,0 +1,14 @@
+package com.example.amphitryon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GestionPlatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestion_plat);
+    }
+}
